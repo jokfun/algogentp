10cf53e6d5d3ec3eb7591a798c90229d15fdd15a
AlexisPister <alexis.pister@hotmail.com>
2018-12-14 23:21:39 +0100

    Algorithm functional

--- mdp.py
+++ mdp.py
@@ -12,7 +12,7 @@ import scipy.stats
 def fct(k):
     return k
 
-def mdp(poss,popmax,generationmax,Tmutation,prob_CO=0.7, c=0.90, freq_bad=0.10):
+def mdp(poss,popmax,generationmax,Tmutation,prob_CO=0.7, c=0.90, freq_bad=0.10, freq_rand=0.10):
     """
         Fonction principale, va effectuer la boucle principale des algos génétiques
         password : le mot de passe à retrouver
@@ -23,9 +23,13 @@ def mdp(poss,popmax,generationmax,Tmutation,prob_CO=0.7, c=0.90, freq_bad=0.10):
         keep : le taux de population choisie lors de la sélection, le reste sera pris au hasard
     """
     # Initialisation
-    genes = []
+    meanFitnesses = []
     bestFitnesses = []
 
+    # Fitness max jamais rencontrée
+    MAX = 0
+
+    genes = []
     for i in range(0,popmax):
         new = []
         for j in range(0,12):
@@ -37,78 +41,96 @@ def mdp(poss,popmax,generationmax,Tmutation,prob_CO=0.7, c=0.90, freq_bad=0.10):
         fit = fitness(genes)
         val = genes[fit.index(max(fit))]
         maxi = max(fit)
-        mini = min(fit)
-        fit_moy = np.mean(fit)
-        bestFitnesses.append(maxi)
-
         word = ""
         for k in val:
             word+=k
-        print(word," score :",maxi, "mean : ", fit_moy, 'min : ', mini)
+        if maxi > MAX:
+            MAX = maxi
+            WORD = word
+        fit_moy = np.mean(fit)
+        bestFitnesses.append(maxi)
+        meanFitnesses.append(fit_moy)
+
+        if i % 50 == 0:
+            print(i, word)
+            print("MAX :", MAX, "max :", maxi, "mean :", fit_moy)
+            #fit.sort()
+            #fit.reverse()
+            #print(fit)
+            #print(genes)
+
 
         # Selection + mutation
-        genes = selection(genes,fit,prob_CO, c, freq_bad)
+        genes = selection(genes,fit,prob_CO, c, freq_bad, freq_rand)
         genes = mutation(genes,Tmutation,poss)
 
         #Si on trouve le bon mot de passe, on quitte la boucle
-        if maxi==2:
-            break
+        if maxi==1:
+            return([WORD, meanFitnesses, bestFitnesses])
+
+    return([WORD, meanFitnesses, bestFitnesses])
 
 
-
-def selection(population, fitnesses, prob_CO, c = 0.90, freq_bad = 0.10):
+def selection(population, fitnesses, prob_CO, c = 0.90, freq_bad = 0.10, freq_rand = 0.10):
     newGeneration = [] # Nouveaux indvidus apres selection
     N = len(population) # Nb d'individus
 
-    # Construction de la matrice des poids (selection exponentielle par le rang)
+    # Construction de la matrice des poids (selection par le rang)
     weights = []
     rangs = scipy.stats.rankdata(fitnesses)
     for r in rangs:
+        # Exponentielle
         weights.append((c - 1) * c**(N - r) / (c**N - 1))
+        # Lineaire
+        #weights.append(2 * (r -1) / (N * (N-1)))
+
     # Applique un scalaire pour que la somme des poids = 1
     weights = np.array(weights) / sum(weights)
+    # Indices des individus les plus mauvais
+    #bad_indexes = np.argsort(weights)[:int(N*freq_bad)]
 
+    # Indices des parents selectionnés aleatoirement, parmis les plus mauvais et avec la roulette
+    inds_randoms = np.random.choice(range(0,N),  int(N * freq_rand))
+    inds_mauvais = np.argsort(weights)[:int(N*freq_bad)]
+    inds_roulette = np.random.choice(range(len(population)), N - len(inds_randoms) - len(inds_mauvais), p=weights)
+
+    # Indices de tous les parents
+    indParents = np.concatenate((inds_randoms, inds_roulette, inds_mauvais))
+    np.random.shuffle(indParents)
+    #print(indParents)
     i = 0
-    while(i < N - freq_bad * N):
-        # Selection
-        indParents = np.random.choice(range(len(population)), 2, p=weights)
-        #print('PARENTS : ', population[indParents[0]], population[indParents[1]])
+    while(i < N):
         r = random.random()
         if r < prob_CO:
-            # Crossing over (random)
-            enfant1 = []
-            enfant2 = []
-            for ind in range(len(population[0])):
-                rand = random.random()
-                if rand < 0.5:
-                    enfant1.append(population[indParents[0]][ind])
-                    enfant2.append(population[indParents[1]][ind])
-                else:
-                    enfant1.append(population[indParents[1]][ind])
-                    enfant2.append(population[indParents[0]][ind])
+            # Crossing over classique ou tirage aleatoire pour chaque caractere a partir des 2 parents
+            r2 = random.random()
+            if r2 < 0.5:
+                enfant1 = []
+                enfant2 = []
+                for ind in range(len(population[0])):
+                    rand = random.random()
+                    if rand < 0.5:
+                        enfant1.append(population[indParents[i]][ind])
+                        enfant2.append(population[indParents[i+1]][ind])
+                    else:
+                        enfant1.append(population[indParents[i]][ind])
+                        enfant2.append(population[indParents[i+1]][ind])
+            else:
+                k = random.randint(1,12)
+                #print(population[indParents[0]])
+                enfant1 = population[indParents[i]][:k] + population[indParents[i+1]][k:]
+                enfant2 = population[indParents[i]][:k] + population[indParents[i+1]][k:]
+                #print('ENFANTS :', enfant1, enfant2)
 
-# =============================================================================
-#             k = random.randint(2,11)
-#             print(population[indParents[0]])
-#             enfant1 = population[indParents[0]][:k] + population[indParents[1]][k:]
-#             enfant2 = population[indParents[1]][:k] + population[indParents[0]][k:]
-#             #print('ENFANTS :', enfant1, enfant2)
-# =============================================================================
         else:
             # Pas de crossing over
-            enfant1 = population[indParents[0]]
-            enfant2 = population[indParents[1]]
+            enfant1 = population[indParents[i]]
+            enfant2 = population[indParents[i+1]]
 
         newGeneration.append(enfant1)
         newGeneration.append(enfant2)
         i += 2
 
-    # Selectionne une proportion freq_bad des plus mauvais individus (pour la diversité)
-    bad_indexes = np.argsort(weights)
-    l = 0
-    while(len(newGeneration) < N):
-         newGeneration.append(population[bad_indexes[l]])
-         l += 1
     return newGeneration
 
 
@@ -135,18 +157,16 @@ def fitness(tab):
 
 def mutation(tab,Tmut,poss):
     """
-        fontion de mutation : chaque position a une chance de Tmut d'etre muté :
-        une chance sur 2 de subir une transformation ou un swap
+        fontion de mutation : chaque position a une chance de Tmut d'etre muté
     """
     for k in range(len(tab)):
         for i in range(len(tab[k])):
             if uniform(0,1)<Tmut:
-                r = uniform(0,1)
-                if r < 0.5:
+                r = random.random()
+                if r < 0.5 or i == len(tab[k]) - 1:
                     tab[k][i] = poss[randint(0,len(poss)-1)]
                 else:
-                    j = range(len(tab[k]))[randint(0, len(tab[k])-1)]
-                    tab[k][i], tab[k][j] = tab[k][j], tab[k][i]
+                    tab[k][i], tab[k][i+1] = tab[k][i+1], tab[k][i]
     return tab
 
 # =============================================================================
@@ -170,26 +190,29 @@ def mutation(tab,Tmut,poss):
 poss=[str(i) for i in range(0,10)]+['_',"A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 #La population max est assez conséquente afin de pouvoir tester beaucoup d'individus
-popmax=200
+popmax = 400
 
 #Le nombre d'iteration max est fixé à 1000, qui est suffisant pour la plupart des exemples
-generationmax = 100000
+generationmax = 10000000
 
 #Le taux de mutation est assez faible mais pas trop : il faut converger vite sans pour autant créer trop d'aléatoire
-Tmutation = 0.03
+Tmutation = 0.15
 
 #Proba de crossing over entre les parents lors de la selection
-prob_CO = 0.80
+prob_CO = 0.60
 
 #Parametre lors de la selection par rang
-c = 0.99
+c = 0.95
 
 #Proportion des individus les plus mauvais gardés à chaque tour
-freq_bad = 0.10
+freq_bad = 0.15
+
+#Proportio d'individus choisis aleatoirement a chaque tour
+freq_rand = 0.10
 
 
 
-mdp(poss,popmax,generationmax,Tmutation,prob_CO, c, freq_bad)
+results = mdp(poss,popmax,generationmax,Tmutation,prob_CO, c, freq_bad, freq_rand)
 
 
 # =============================================================================
